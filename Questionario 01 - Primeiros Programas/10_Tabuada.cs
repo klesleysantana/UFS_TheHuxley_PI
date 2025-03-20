6bc33a936c36a3d69a18b6bfe9e2f6e61506c1c3
Yelselk <klesleysantana@gmail.com>
2025-03-19 23:46:31 -0300

    First exercises and test the git

---
+++ Questionario 01 - Primeiros Programas/10_Tabuada.cs
@@ -0,0 +1,7 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i <= 10; i++)
+{
+    int multi = number * i;
+    Console.WriteLine($"{number} X {i} = {multi}");
+}
---
+++ Questionario 01 - Primeiros Programas/1_MediaDe2Notas.cs
@@ -0,0 +1,6 @@
+﻿double score1 = double.Parse(Console.ReadLine());
+double score2 = double.Parse(Console.ReadLine());
+
+double avarage = (score1 + score2) / 2;
+
+Console.WriteLine(avarage.ToString("F2"));
